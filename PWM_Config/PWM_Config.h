b710f3f2407fca920e68472e15a748769f9c6793
luoyang <ly223038@163.com>
2015-07-01 23:31:28 +0800

    PR_Input Successfully

---
+++ PWM_Config/PWM_Config.h
@@ -0,0 +1,31 @@
+/*
+*PB9 BLUE PB8 GREEN PB7 RED 
+*
+*
+*
+*/
+
+
+#include "stm32f10x_lib.h"
+/*
+DO 262 523 1046
+RE 294 587 1175
+MI 330 659 1318
+FA 349 698 1397
+SO 392 784 1568
+LA 440 880 1760
+SI 494 988 1967
+
+*/
+#define DO_L   275
+#define RE_L   245
+#define MI_L   218
+#define FA_L   206
+#define SO_L   184
+#define LA_L   164
+#define SI_L   146
+
+
+
+
+void LED_PWM_Config();
---
+++ user/main.c
@@ -0,0 +1,78 @@
+
+#include "stm32f10x_lib.h"
+#include "RCC_Configuration.h"  //œµÕ≥ ±÷”≈‰÷√
+#include "USART_Configuration.h"
+#include "GPIO_Config.h"
+#include "PWM_Config.h"
+#include "stm32f10x_exti.h"
+#include "stm32f10x_nvic.h"
+#include "stm32f10x_gpio.h"
+#include <stdio.h>
+
+
+int main(void)
+	{
+		int PR_Input[3] = {0,0,0};
+
+		RCC_Configuration();
+		USART_Configuration();
+    GPIO_Config();
+		LED_PWM_Config();
+		
+    /*TEST*/
+		TIM_Cmd(TIM4,ENABLE);
+		TIM_SetCompare2(TIM4,999);
+		TIM_SetCompare3(TIM4,999);
+		TIM_SetCompare4(TIM4,0);
+		
+		while(1)
+		{
+			PR_Input[0] = Get_PRInput();
+			printf("%d\r\n",PR_Input[0]);
+			
+			
+				switch(PR_Input[1])
+				{
+					case PRINPUT_1:
+						break;
+					case PRINPUT_2:
+						break;
+					case PRINPUT_3:
+						break;
+					case PRINPUT_4:
+						break;
+					case PRINPUT_5:
+						break;
+					case PRINPUT_6:
+						break;
+					case PRINPUT_7:
+						break;
+					case PRINPUT_8:
+						break;
+					case PRINPUT_9:
+						break;
+					case PRINPUT_10:
+						break;
+					case PRINPUT_11:
+						break;
+					case PRINPUT_12:
+						break;
+					case PRINPUT_13:
+						break;
+					case PRINPUT_14:
+						break;
+					case PRINPUT_15:
+						break;
+					
+						
+					default:
+					break;
+				}
+
+				
+		 }
+	}
+	
+
+
+	
